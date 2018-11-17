547c764bedb4abc77e1674296c7241889dadfb8a
Adi <adibakija35@hotmail.com>
2018-11-17 23:39:17 +0000

    Added .env variables and setup database

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,29 @@
+require('dotenv').config()
 const express = require('express');
-const bodyParser  = require("body-parser");
+const bodyParser = require("body-parser");
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
 
+  pool: {
+    max: 10,
+    min: 2,
+    acquire: 30000,
+    idle: 10000
+  },
+
+});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection to sequelize has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
 
 const app = express();
 
