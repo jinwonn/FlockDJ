2d5faefae472c833ef3e8d9db032d98db121b1c2
Daniel <d.t.nguyen@live.com>
2018-11-25 20:44:27 +0000

    some changes not added to last commit

--- src/client/views/Room.jsx
+++ src/client/views/Room.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import '../styles/room.css';
 import Chat from './components/Chat.jsx'
 import Player from './components/Player.jsx'
-import spotifyhelper from '../assets/spotify-helper'
 
 export default class Room extends Component {
   constructor(props, context) {
@@ -11,7 +10,6 @@ export default class Room extends Component {
     const { chatHistory } = props			
     this.state = {
     // chatHistory, this is if you want to show previous chat history
-      spotifyhelper: spotifyhelper(),
       chatHistory: [],
       username: null,
       roomname: this.props.roomname
@@ -21,13 +19,12 @@ export default class Room extends Component {
   componentDidMount() {
     console.log("Room.jsx room name:", this.state.roomname)
     this.props.messageHandler(this.onMessageReceived)
-		this.props.playHandler(this.state.spotifyhelper.play_Song)
 		
-		const script = document.createElement("script");
-		script.src = 'https://sdk.scdn.co/spotify-player.js';
-		script.src = 'src/client/assets/player.js';
-		script.async = true;
-		document.body.appendChild(script)
+		// const script = document.createElement("script");
+		// script.src = 'https://sdk.scdn.co/spotify-player.js';
+		// script.src = 'src/client/assets/player.js';
+		// script.async = true;
+		// document.body.appendChild(script)
 
     fetch('/api/getUsername')
       .then(res => res.json())
@@ -55,7 +52,7 @@ export default class Room extends Component {
 	      	<Chat user={this.state.username} room={this.state.roomname} chatHistory={this.state.chatHistory}/>
 	      </div>
         <div>
-          <Player room={this.state.roomname} />
+          <Player room={this.state.roomname} playHandler={this.props.playHandler}/>
         </div>
       </div>
     );
