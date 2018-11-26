11a89f5cb2878216161741a261d29e3a5b4e3804
Daniel <d.t.nguyen@live.com>
2018-11-26 16:03:54 +0000

    added functionality to display currently playing song information

---
+++ src/client/views/components/spotify/NowPlaying.jsx
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+export default class NowPlaying extends Component {
+  render() {
+    let {
+      playerState,
+      playerState: { position: position_ms }
+    } = this.props;
+    let {
+      id,
+      uri: track_uri,
+      name: track_name,
+      duration_ms,
+      artists: [{
+        name: artist_name,
+        uri: artist_uri
+      }],
+      album: {
+        name: album_name,
+        uri: album_uri,
+        images: [{ url: album_image }]
+      }
+    } = playerState.track_window.current_track;
+
+    return (
+      <div>
+      <img className='album-art' src={album_image} alt={track_name} />
+	    			<span className='song-description'>
+	    				<h3 href={track_uri}>{track_name}</h3>
+	    				<p href={artist_uri}>{artist_name}</p>
+	    				<p href={album_uri}>{album_name}</p>
+	    			</span>
+      </div>
+    );
+  }
+}
