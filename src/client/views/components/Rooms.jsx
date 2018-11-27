4c42aeb95dd731fc37b6673478db62c08d04b9ec
Daniel <d.t.nguyen@live.com>
2018-11-27 15:59:37 +0000

    resolved conflicts with merging styling

--- src/client/views/components/Rooms.jsx
+++ src/client/views/components/Rooms.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
 
-export default ({ room, onEnter }) => (
-    <div className="card" style={{width: '18rem'}} onClick={onEnter}>
-      <img className="card-img-top" src="https://charliebarnett.com/wp-content/themes/soundcheck/images/default-album-artwork.png" alt="Card image cap"></img>
-      <div className="card-body">
-        <h6 className="card-title">{room.name}</h6>
+class Rooms extends Component {
+  constructor(props, context) {
+    super(props, context)
+    
+    this.state = {
+      room: this.props.room
+    };
+    
+    this.routeChange = this.routeChange.bind(this);
+  }
+
+  routeChange() {
+    let path = `/${this.state.room.name}`;
+    this.props.history.push(path);
+  }
+
+
+  render() {  
+    return (
+      <div className="card" style={{width: '18rem'}} onClick={this.routeChange}>
+        <img className="card-img-top" src="https://charliebarnett.com/wp-content/themes/soundcheck/images/default-album-artwork.png" alt="Card image cap"></img>
+        <div className="card-body">
+          <h6 className="card-title">{this.state.room.name}</h6>
+        </div>
       </div>
-    </div>
-)
+    )
+  }
+}
+export default withRouter(Rooms);
