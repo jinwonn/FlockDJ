b510a849b373086d05441d2485f6cd0d29e8c791
Adi <adibakija35@hotmail.com>
2018-11-27 05:32:25 +0000

    Styled browse page

--- src/client/views/components/Rooms.jsx
+++ src/client/views/components/Rooms.jsx
@@ -4,11 +4,10 @@ export default ({ room, onEnter }) => (
     /*<button onClick={onEnter}>
         {room.name} room
     </button>*/
-    <div className="card" style={{width: '18rem'}}>
+    <div className="card" style={{width: '18rem'}} onClick={onEnter}>
       <img className="card-img-top" src="https://charliebarnett.com/wp-content/themes/soundcheck/images/default-album-artwork.png" alt="Card image cap"></img>
       <div className="card-body">
-        <h5 className="card-title">{room.name}</h5>
-        <a onClick={onEnter} className="btn btn-primary">Enter Room</a>
+        <h6 className="card-title">{room.name}</h6>
       </div>
     </div>
 )
