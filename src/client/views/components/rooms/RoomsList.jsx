57b7d6508dc36339ac16c736b6e57a69515ae7ee
jrajan20 <jai_20j@hotmail.com>
2018-11-27 21:15:26 +0000

    moved createroom in roomlist

--- src/client/views/components/rooms/RoomsList.jsx
+++ src/client/views/components/rooms/RoomsList.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Rooms from './Rooms.jsx'
 import '../../../styles/browse.css';
+import CreateRoom from '../CreateRoom.jsx';
 
 export default ({
   rooms
 }) => (
   <div>
+  <CreateRoom className="create-room"/>
     <div className="card-layout">
       <div className="card-deck">
         {
