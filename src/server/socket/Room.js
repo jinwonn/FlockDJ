03d2ca6dd95fe68d3b1f2d4d8d42f169f1473d72
Daniel <d.t.nguyen@live.com>
2018-11-27 00:13:02 +0000

    cleaned up socket server code

--- src/server/socket/Room.js
+++ src/server/socket/Room.js
@@ -9,10 +9,40 @@ module.exports = ({ name }) => {
   let staged = null;
   let playing = null;
 
+  // ========== Room Functions ==========
+  
+  function addUser(client) {
+    members.set(client.id, client);
+  }
+
+  function removeUser(client) {
+    members.delete(client.id);
+  }
+
+  function serialize() {
+    return {
+      name,
+      numberofMembers: members.size
+    };
+  }
+
+  // ========== Chat Functions ==========
+
   function broadcastMessage(message) {
     console.log('broadcasting: (', message, ') to members');
     members.forEach(m => m.emit('message', message));
   }
+
+  function addEntry(entry) {
+    chatHistory = chatHistory.concat(entry);
+  }
+
+  function getChatHistory() {
+    console.log('getting chat history, got:', chatHistory.slice());
+    return chatHistory.slice();
+  }
+
+  // ========== Spotify Functions ==========
 
   function broadcastSong() {
     console.log('broadcasting to play : (', playing, ') to members');
@@ -54,41 +84,10 @@ module.exports = ({ name }) => {
     if (newQueue) {
       roomqueue = newQueue;
       history = [];
-      console.log("new queue set success");
       // if nothing is playing, run the `play` function (otherwise, the play function should
       // already be running, and will roll with the new queue).
       if (!playing && !staged) play();
     }
-  }
-
-  function addEntry(entry) {
-    console.log('adding entry', entry);
-    chatHistory = chatHistory.concat(entry);
-    console.log('chat histtory:', chatHistory);
-  }
-
-  function getChatHistory() {
-    console.log('getting chat history, got:', chatHistory.slice());
-    return chatHistory.slice();
-  }
-
-  function addUser(client) {
-    // console.log('adding', client.id, 'to room.', '# of members of room:', members.size)
-    members.set(client.id, client);
-    console.log("members:",members)
-    console.log('addUser(client). now # of members of room:', members.size, 'room', {name});
-  }
-
-  function removeUser(client) {
-    // console.log('removing user from room:', client.id)
-    members.delete(client.id);
-  }
-
-  function serialize() {
-    return {
-      name,
-      numberofMembers: members.size
-    };
   }
 
 
